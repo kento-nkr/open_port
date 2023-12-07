4cdd00f2bd318acb48ad460f9a6d398236a8712b
kento tokura <k-tokura@nkr-group.com>
2023-12-07 09:40:52 +0900

    add port set function

--- open_port.ps1
+++ open_port.ps1
@@ -5,10 +5,15 @@ $port = 3000
 # WSL2のディストリビューション名 (※適宜変更)
 $distName = "Ubuntu"
 
-# "clean" オプションが指定された場合は過去の設定をリセット
+# 引数の一個目が数値だった場合、port番号として解釈する
+if ($args[0] -match '^\d+$') {
+    $port = [int]$args[0]
+}
+
+# "clear" オプションが指定された場合は過去の設定をすべて削除
 if ($args[0] -eq "clear") {
-    Remove-NetFirewallRule -DisplayName "WSL 2 Firewall Unlock"
-    netsh interface portproxy delete v4tov4 listenport=$port listenaddress=*
+    Get-NetFirewallRule -DisplayName "WSL 2 Firewall Unlock" | Remove-NetFirewallRule
+    netsh interface portproxy reset
     return
 }
 
@@ -19,6 +24,20 @@ if ($args[0] -eq "status") {
 }
 
 # Windows Defenderに穴あけ
-New-NetFireWallRule -DisplayName "WSL 2 Firewall Unlock" -Direction Inbound -LocalPort $port -Action Allow -Protocol TCP
+New-NetFireWallRule -DisplayName "WSL 2 Firewall Unlock" -Direction Outbound -LocalPort $port -Action Allow -Protocol TCP
+New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $port -Action Allow -Protocol TCP
 # WSL2の現在のIPに対するポートフォワーディング設定
 netsh interface portproxy add v4tov4 listenport=$port listenaddress=* connectport=$port connectaddress=(wsl -d $distName -e hostname -I).trim()
+
+
+# 現在のユーザーが管理者権限を持っていない場合に、スクリプトを管理者権限で再実行するための処理です。
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+# WSL 2 インスタンスの IP アドレスを取得します。
+# bash.exe を使って、ip r コマンドを実行し、結果からIPアドレスを抽出します。
+# IP アドレスが取得できない場合、スクリプトは終了します。
+$ip = bash.exe -c "ip r |tail -n1|cut -d ' ' -f9"
+if ( ! $ip ) {
+    echo "The Script Exited, the ip address of WSL 2 cannot be found";
+    exit;
+}
